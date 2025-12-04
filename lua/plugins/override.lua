6cadbd893697bd7aad8948c26dbdf8be72015159
safonov-maksim <maksim.safonov@indriver.com>
2025-12-04 17:25:00 +0200

    update file path in lualine, disable strict gofumt formatter for go

--- lua/plugins/override.lua
+++ lua/plugins/override.lua
@@ -13,4 +13,37 @@ return {
       scroll = { enabled = false },
     },
   },
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = function(_, opts)
+      -- Disable statusline code symbol (navic)
+      opts.sections.lualine_c = {
+        { "filename", path = 1, shorting_target = 0 }, -- relative file path with disabled shortening
+      }
+
+      return opts
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        gopls = {
+          settings = {
+            gopls = {
+              gofumpt = false, -- disable strict formatter gofumpt
+            },
+          },
+        },
+      },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        go = { "gofmt" },
+      },
+    },
+  },
 }
